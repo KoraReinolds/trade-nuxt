fa483c8c5891ea561d67c0eeedf2edf6ca7eef30
KoraReinolds <s.shurin94@gmail.com>
2023-03-28 01:14:55 +0300

    Load candles from db Loading candles from db increased performance by 3 times - add to '/[date]' endpoint logic for loading candles from db using a query param

--- server/api/candles/[figi]/[interval]/[date]/index.get.ts
+++ server/api/candles/[figi]/[interval]/[date]/index.get.ts
@@ -1,21 +1,43 @@
 import { CandleInterval } from "tinkoff-invest-api/cjs/generated/marketdata";
 import { Helpers } from "tinkoff-invest-api";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { TinkoffAPI } from "~~/classes/Tinkoff";
 import { IntervalKeys, IntervalMap } from "~~/types/IntervalMap";
 
 const api = new TinkoffAPI();
+const prisma = new PrismaClient();
+const loadCandlesFromDB = async (data: Prisma.CandlesFindManyArgs) => {
+  return await prisma.candles.findMany(data);
+};
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
+  const db = !!(query.db || false);
   const offset = (query.offset || "1d").toString();
   const params = event.context.params;
   const figi = params?.figi || "";
   const interval = params?.interval as IntervalKeys;
   const date = params?.date || "";
+  const { from, to } = Helpers.fromTo(offset, new Date(date));
+
+  if (db) {
+    return {
+      candles: await loadCandlesFromDB({
+        where: {
+          shares: figi,
+          time: {
+            lte: to,
+            gte: from,
+          },
+        },
+      }),
+    };
+  }
 
   return await api.getCandles({
     figi,
     interval: CandleInterval[IntervalMap[interval]],
-    ...Helpers.fromTo(offset, new Date(date)),
+    from,
+    to,
   });
 });
