e6fe287b0352b579cf48be39c743ca6b24bb7dc5
KoraReinolds <s.shurin94@gmail.com>
2023-04-01 20:35:14 +0300

    Fragment shader line thickness fix - scale uv so that only values from 0 to 1 are displayed - thickness in this case depends only from resolution

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,6 +1,6 @@
 #define RGB_DARK vec3(.1)
 #define RGB_GRAY_2 vec3(.2)
-#define PIXEL_SIZE 1./u_resolution.x
+#define PIXEL_SIZE 1.
 
 varying vec2 st;
 uniform vec2 u_resolution;
@@ -14,12 +14,12 @@ float stroke(float v,float c,float w){
   return fill(v,c+w/2.)-fill(v,c-w/2.);
 }
 
-float grid(vec2 uv,vec2 scale,float thickness){
+float grid(vec2 uv,vec2 scale,vec2 thickness){
   vec2 grid_uv=uv;
   grid_uv=fract(grid_uv*scale);
-  vec2 pixel_size=thickness*scale;
-  float grid_x=fill(grid_uv.x,pixel_size.x);
-  float grid_y=fill(grid_uv.y,pixel_size.y);
+  vec2 ps=thickness*scale;
+  float grid_x=fill(grid_uv.x,ps.x);
+  float grid_y=fill(grid_uv.y,ps.y);
   
   return max(grid_x,grid_y);
 }
@@ -29,10 +29,16 @@ void main(){
   vec2 uv=st;
   uv.x=1.-uv.x;
   float aspect=u_resolution.x/u_resolution.y;
+  if(aspect>1.){
+    uv.y*=aspect;
+  }else{
+    uv.x/=aspect;
+  }
+  vec2 pixel_size=PIXEL_SIZE/u_resolution;
   float grid_scale=max(1.,aspect);
   
   scene=mix(scene,RGB_GRAY_2,
-    grid(uv,u_grid,PIXEL_SIZE)
+    grid(uv,u_grid,pixel_size)
   );
   
   gl_FragColor=vec4(scene,1.);
